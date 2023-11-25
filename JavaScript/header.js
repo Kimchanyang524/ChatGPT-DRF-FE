d79a731f755fe378b5b08618e8fc0700f973731d
Chanyang Kim <cks402@naver.com>
2023-11-25 08:37:06 +0000

    🐛 fix: javascript include 수정

--- JavaScript/header.js
+++ JavaScript/header.js
@@ -1,11 +1,10 @@
-let js = document.querySelector('header.includeJs');
-
-(function () {
+document.addEventListener("DOMContentLoaded", function () {
     const sign = document.querySelector('#sign');
     const token = sessionStorage.getItem('token');
-    if (token) {
+
+    if (sign && token) {
         sign.innerHTML = `<a class="nav-link" onclick="logout()"><i class="fa-solid fa-right-from-bracket"></i></a>`;
-    } else {
+    } else if (sign) {
         sign.innerHTML = `<button class="nav-link" href="/accounts/login"><i class="fa-solid fa-right-to-bracket"></i></button>`;
     }
-})();
+})
